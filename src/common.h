dd79e3aaa0a38bf4e41b404ee42dfa1bae6a2c10
ElemenTP <fpjraidden@outlook.com>
2023-03-03 19:27:03 +0800

    chore: Improved version number detection and display of Windows

--- src/common.h
+++ src/common.h
@@ -51,10 +51,18 @@
 #ifdef __MINGW32__
 #  define WIN32_LEAN_AND_MEAN
 #  ifndef WINVER
+#    ifdef _WIN64
+#    define WINVER 0x601
+#    else
 #    define WINVER 0x501
+#    endif
 #  endif // !WINVER
 #  ifndef _WIN32_WINNT
+#    ifdef _WIN64
+#    define _WIN32_WINNT 0x601
+#    else
 #    define _WIN32_WINNT 0x501
+#    endif
 #  endif // _WIN32_WINNT
 #  ifdef HAVE_WINSOCK2_H
 #    ifndef FD_SETSIZE
